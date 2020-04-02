bf065ffe9bf91133be54d34d8bd0dd34682b3fcc
Rohit Patnaik <quanticle@quanticle.net>
2020-04-02 15:37:53 -0500

    2020-04-02

--- src/pmwiki_to_markdown.py
+++ src/pmwiki_to_markdown.py
@@ -1,2 +1,10 @@
-def convert_heading(line):
-    pass
+import re
+
+HEADING_RE = re.compile(r'^(!+)')
+def is_heading(line: str):
+    return HEADING_RE.match(line)
+
+def convert_heading(heading: str, match: re.match) -> str:
+    heading_level = len(match.group(1))
+    return re.sub(HEADING_RE, '#' * heading_level, heading)
+
